83c56751be785a50e1ef2f1eacbf45762efeb3e8
Jeremy McPeak <jwmcpeak@gmail.com>
2013-04-14 21:25:50 -0500

    Added TelevisionTest file

---
+++ McPeak.Media.Aquos.Test/TelevisionTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace McPeak.Media.Aquos.Test
+{
+    [TestClass]
+    public class TelevisionTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
